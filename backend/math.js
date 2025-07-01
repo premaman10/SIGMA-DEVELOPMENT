b582de02777ad914aa4df578c7ef70de333f7c9d
sakshi <119154881+premaman10@users.noreply.github.com>
2025-07-01 09:11:55 +0530

    Add files Via Upload

---
+++ backend/math.js
@@ -0,0 +1,7 @@
+const sum = (a,b)=>a+b;
+const PI = 3.14;
+let obj = {
+    sum: sum,
+    PI: PI,
+};
+module.exports = obj;
---
+++ backend/sript.js
@@ -0,0 +1,3 @@
+const math = require("./math");
+console.log(math.sum(9,8));
+console.log(math.PI);
